d252b5ef36fc1e3192f5730574f77e2621af00b8
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2023-11-02 13:54:27 +0000

    Bumped lean toolchain, added doc-gen dependency

--- Isotope.lean
+++ Isotope.lean
@@ -1,3 +1,1 @@
--- This module serves as the root of the `Isotope` library.
--- Import modules here that should be built as part of the library.
-import «Isotope».Basic
+import Isotope.Basic
--- lakefile.lean
+++ lakefile.lean
@@ -10,3 +10,6 @@ require mathlib from git
 @[default_target]
 lean_lib «Isotope» where
   -- add any library configuration options here
+
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
