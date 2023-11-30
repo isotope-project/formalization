e4a8e890aa565a2a106947c24c4db75793ed8eb2
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2023-11-30 09:59:33 +0000

    Tap

--- Isotope/Syntax/Intrinsic/Subst.lean
+++ Isotope/Syntax/Intrinsic/Subst.lean
@@ -10,11 +10,3 @@ inductive Ctx.subst {T: Type u} [HasLin T] (F: Type u) [InstructionSet F T]
     (HΘx: HasLin.lin Θx q)
     (t: Term F Θx p A q)
     : subst F Θ (⟨q, A⟩::Γ)
-
-def Ctx.subst.wk_src {T: Type u} [HasLin T] {F: Type u} [InstructionSet F T]
-  {Θ Θ' Γ: Ctx T} (H: Θ'.wk Θ): subst F Θ Γ -> subst F Θ' Γ
-  | nil HΘ => nil (H.comp HΘ)
-  | cons S HΘΓ HΘx t =>
-    let ⟨_, _, S', Wl, Wr⟩ := S.distribute_left H
-    sorry
-    -- cons S' (HΘΓ.wk_src Wl) sorry sorry
