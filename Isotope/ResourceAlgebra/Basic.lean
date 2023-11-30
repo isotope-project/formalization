fe1d086b5052cc0aa13c318a9cc0511ef4a511e5
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2023-11-30 10:11:33 +0000

    Tap

--- Isotope/ResourceAlgebra/Basic.lean
+++ Isotope/ResourceAlgebra/Basic.lean
@@ -9,9 +9,10 @@ class ResourceAlgebra (T: Type u) extends
   valid: T -> Prop
   valid_one: valid 1
   valid_zero: valid 0
-  valid_sum_left: ∀ x y, valid (x + y) → valid x
-  valid_sum_right: ∀ x y, valid (x + y) → valid y
+  valid_add_left: ∀ x y, valid (x + y) → valid x
+  valid_add_right: ∀ x y, valid (x + y) → valid y
   zero_le_one: zero ≤ one
+  add_increasing: ∀x y: T, x + y ≥ y
   -- TODO: allow purely "ghostly" values?
   -- or would this be by making 1 invalid?
   zero_ne_one: zero ≠ one
